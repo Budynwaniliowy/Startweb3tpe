65ef6cf7d2418efd22279ba5df83ba994dfc8df8
Budynwaniliowy <156786393+Budynwaniliowy@users.noreply.github.com>
2025-01-22 13:23:34 +0100

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,11 +1,22 @@
 <?php
 
 $menu = array (
-	'Dashboard' => array('page' => 'index', 'name' => 'Pulpit'),
-	'Users' => array('page' => 'users', 'name' => 'Użytkownicy',
-		'actions' => array (
-			0 => array ('action' => 'index', 'title' => 'Lista użytkowników'),
-			1 => array ('action' => 'add', 'title' => 'Dodaj użytkownika')
-		)
-	)
+    'Dashboard' => array('page' => 'index', 'name' => 'Pulpit'),
+    'News' => array(
+        'page' => 'news',
+        'name' => 'News',
+        'actions' => array (
+            0 => array ('action' => 'index', 'title' => 'Lista aktualności'),
+            1 => array ('action' => 'add', 'title' => 'Dodaj aktualność')
+        )
+    ),
+    'Users' => array(
+        'page' => 'users',
+        'name' => 'Użytkownicy',
+        'actions' => array (
+            0 => array ('action' => 'index', 'title' => 'Lista użytkowników'),
+            1 => array ('action' => 'add', 'title' => 'Dodaj użytkownika')
+        )
+    )
 );
+?>
